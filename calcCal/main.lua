274fdb29523bbd83c6b438fb5037bf4ecabc3903
dicene <ieadkins@gmail.com>
2016-05-24 13:42:34 -0400

    Created by Coding Ground Client

---
+++ calcCal/main.lua
@@ -0,0 +1,13 @@
+function calcCal(fat, carbs, prot)
+    local total = fat + carbs + prot
+    local fatCal = fat * 9
+    local carbsCal = carbs * 4
+    local protCal = prot * 4
+    local totalCal = fatCal + carbsCal + protCal
+    print(string.format("Fat   : %.0f%% %ic", fat/total*100, fatCal))
+    print(string.format("Carbs : %.0f%% %ic", carbs/total*100, carbsCal))
+    print(string.format("Prot  : %.0f%% %ic", prot/total*100, protCal))
+    print(string.format("Calories  : %ic", totalCal))
+end
+
+calcCal(36, 107, 44)
